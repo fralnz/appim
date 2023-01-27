3dc6194d3680f30f35802c079420dc4b3f504395
WalkingGarbage <fra@void.station>
2023-01-27 15:05:24 +0100

    better ui and icon finding

--- appim.sh
+++ appim.sh
@@ -19,9 +19,9 @@ echo "Argument: $file"    #prints the file name
 
 if [[ $file == *.AppImage ]]    #checks if the file is an AppImage
 then
-  echo "The argument is an appimage!"
+  echo "[OK] AppImage recognized"
 else
-  echo "The argument is NOT an appimage!"
+  echo "[ERR] The argument is NOT an appimage"
   exit 0
 fi
 
@@ -30,10 +30,26 @@ uninstall(){
   echo "removed appimage"
   rm $HOME/.local/share/applications/$desktopentry
   echo "removed desktop entry"
-  rm $HOME/.local/share/icons/filename.png
+  rm $HOME/.local/share/icons/$filename.png
   echo "removed icon"
   echo "$filename uninstalled"
   exit 0
+}
+
+geticon(){
+  cd squashfs-root
+  count=`ls -1 *icon.png 2>/dev/null | wc -l`
+  cd ..
+
+  if [[ -f squashfs-root/usr/share/icons/hicolor/512x512/apps/*.png ]]; then
+    cp squashfs-root/usr/share/icons/hicolor/512x512/apps/*.png $icondir/$filename.png
+    echo "[OK] Icon found"
+  elif [[ $count != 0 ]]; then
+    cp squashfs-root/*icon.png $icondir/$filename.png
+    echo "[OK] Icon found"
+  else
+    echo "[WARN] Icon not found"
+  fi
 }
 
 install(){
@@ -57,10 +73,12 @@ fi
 chmod +x $file           #makes the file executable
 cp $file $tempdir
 cd $tempdir
-./$filename --appimage-extract
+./$filename --appimage-extract &>/dev/null
+echo "[OK] AppImage extracted"
 # Icon copy
-cp squashfs-root/usr/share/icons/hicolor/512x512/apps/*.png $icondir/$filename.png
 mv $filename $appdir
+echo "[OK] Moved AppImage"
+geticon
 
 # .desktop file creation
 touch $filenameonly.desktop
@@ -70,10 +88,16 @@ echo "Name=$filenameonly" >> $desktopentry
 echo "Exec=$appdir/$filename" >> $desktopentry
 echo "Icon=$icondir/$filename.png" >> $desktopentry
 
+echo "[OK] Desktop entry created"
+
 mv $desktopentry $HOME/.local/share/applications
 
+echo "[OK] Moved desktop entry"
 # cleanup
 rm -rf $tempdir
+echo "[OK] Cache cleared"
+
+echo "[DONE] AppImage installed"
 }
 
 while getopts 'iu' OPTION; do
